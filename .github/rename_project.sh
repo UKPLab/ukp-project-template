0efbff33e075a112308b8660ecfeb3aa2a885836
akatief <f.tib29@gmail.com>
2024-02-21 15:15:43 +0100

    fix: update rename_project to not affect rename action

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -17,20 +17,24 @@ echo "Description: $description";
 echo "Renaming project..."
 
 original_author="UKPLab"
-original_name="ukp-project-template"
+original_name="ukp_project_template"
 original_urlname="ukp-project-template"
 original_description="The official template for new Python projects at UKP Lab"
-# for filename in $(find . -name "*.*") 
-for filename in $(git ls-files) 
-do
-    sed -i "s/$original_author/$author/g" $filename
-    sed -i "s/$original_name/$name/g" $filename
-    sed -i "s/$original_urlname/$urlname/g" $filename
-    sed -i "s/$original_description/$description/g" $filename
-    echo "Renamed $filename"
+# Iterate over all files in the repository
+git ls-files | while read -r filename; do
+    # Exclude .github/workflows/rename_project.yml from renaming
+    if [[ "$filename" != ".github/workflows/rename_project.yml" ]]; then
+        sed -i "s/$original_author/$author/g" "$filename"
+        sed -i "s/$original_name/$name/g" "$filename"
+        sed -i "s/$original_urlname/$urlname/g" "$filename"
+        sed -i "s/$original_description/$description/g" "$filename"
+        echo "Renamed $filename"
+    else
+        echo "Skipping $filename"
+    fi
 done
 
-mv ukp_project_template $name
+mv "$original_name" "$name"
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
